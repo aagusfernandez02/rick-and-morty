6a4542760493e4d216a2568964d6db9b8192b5d3
Agustín Fernandez <af.agusfernandez02@gmail.com>
2024-10-04 15:52:47 -0300

    initial scaffold

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Header from './components/Header.vue'; 
+</script>
+
+<template>
+  <main>
+    <Header />
+  </main>
+</template>
+
+<style scoped lang="scss">
+main {
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+}
+</style>
